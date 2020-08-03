1f775f8b318358b9d0bc0ee2131c4324a1b040c9
Jacob Schwartz <jacob@jtschwartz.com>
2020-08-02 21:43:08 -0400

    zipcode test

--- functions/sms.js
+++ functions/sms.js
@@ -1,12 +1,36 @@
-const qs = require('querystring')
+const qs = require("querystring")
+
+const weatherApi = "***REMOVED***"
+
+
+async function weather(zipCode) {
+	const [lat, lon] = require('../us-zip-code-latitude-and-longitude.json')[zipCode]
+	
+	return await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,daily&units=imperial&appid=${weatherApi}`)
+		.then(response => response.json())
+		.then(data => {
+			return [data["lat"], data["lng"]]
+		}).catch(() => {
+			return null
+		})
+}
 
 exports.handler = async (event) => {
 	const content = qs.parse(event.body)
-	const cmd = content.Body.split(' ')
+	const [cmd, ...args] = content.Body.split(" ")
+	let response
+	
+	// switch (cmd.toUpperCase()) {
+	// 	case "JEEP":
+	// 	case "RAIN":
+	// 	case "STORM":
+	// 	case "WEATHER":
+			response = await weather(args[0])
+	// }
 	
 	const xmlReponse = `<Response>
 							<Message>
-								${cmd}
+								${[cmd, response].flat(Infinity)}
 							</Message>
 						</Response>`
 	return {
@@ -14,6 +38,6 @@ exports.handler = async (event) => {
 		headers:    {
 			"Content-Type": "text/xml"
 		},
-		body: xmlReponse,
+		body:       xmlReponse
 	}
 }
